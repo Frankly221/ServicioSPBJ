c76bc43f5d46be08ae38624fcc5e696c663eaba0
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-09-24 16:04:07 -0500

    refactorizando y agregando la funcion de agregar listado de pago

--- src/main/java/com/App/APP/Repositorio/PagoRepository.java
+++ src/main/java/com/App/APP/Repositorio/PagoRepository.java
@@ -1,11 +1,15 @@
 package com.App.APP.Repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.App.APP.Entity.Pago;
 
 @Repository
 public interface PagoRepository extends JpaRepository<Pago, Integer> {
-
+    @Query("SELECT p FROM Pago p WHERE p.diagnostico.idhc = :id")
+    public List<Pago> findByDiagnosticoId(Integer id);
 }
--- src/main/java/com/App/APP/Service/PagoService.java
+++ src/main/java/com/App/APP/Service/PagoService.java
@@ -1,28 +1,22 @@
 package com.App.APP.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.App.APP.DTO.PagoDTO;
+import com.App.APP.Entity.Diagnostico;
 import com.App.APP.Entity.Pago;
 import com.App.APP.Mapper.PagoMapper;
 import com.App.APP.Repositorio.PagoRepository;
 
 @Service
 public class PagoService {
-     @Autowired
+    @Autowired
     private PagoRepository pagoRepository;
-
-    public List<PagoDTO> SearchAllPago() {
-        List<Pago> pagoList = pagoRepository.findAll();
-        if (pagoList.isEmpty()) {
-            return null;
-        }
-        return pagoList.stream().map(PagoMapper::DatosToDTO).collect(Collectors.toList());
-    }
 
     public void SavePago(PagoDTO pagoDTO) {
         Pago Pago = PagoMapper.DatosToEntity(pagoDTO);
@@ -43,8 +37,32 @@ public class PagoService {
     }
 
     private Pago searchEntityPago(int idPago) {
-        return pagoRepository.findById(idPago)
-                .orElseThrow(() -> new RuntimeException("Pago No encontrado con el id " + idPago));
+        Pago PagoEncontrado = pagoRepository.findById(idPago).orElse(null);
+        if (PagoEncontrado == null) {
+            throw new RuntimeException("El pago no existe");
+        }
+        return PagoEncontrado;
+    }
+
+    public void actualizarListaPagos(List<Pago> pagos, int idDiagnostico) {
+        List<Pago> listaOriginal = pagoRepository.findByDiagnosticoId(idDiagnostico);
+        Set<Integer> originalIds = listaOriginal.stream().map(Pago::getId_pago).collect(Collectors.toSet());
+
+        for (Pago pagOriginal : listaOriginal) {
+            if (pagos.stream().noneMatch(p -> p.getId_pago() == pagOriginal.getId_pago())) {
+                pagoRepository.deleteById(pagOriginal.getId_pago());
+            }
+        }
+
+        for (Pago pago : pagos) {
+            if (pago.getPago() > 0) {
+                if (originalIds.contains(pago.getId_pago()) || pago.getId_pago() == 0) {
+                    Diagnostico diag = Diagnostico.builder().idhc(idDiagnostico).build();
+                    pago.setDiagnostico(diag);
+                    pagoRepository.save(pago);
+                }
+            }
+        }
     }
 
 }
